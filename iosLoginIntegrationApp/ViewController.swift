aec10458f40d0e10d66b11e12334e517779abb03
Anindya <akpaul@bu.edu>
2016-10-02 17:02:51 -0400

    Updated View Controller

--- iosLoginIntegrationApp/ViewController.swift
+++ iosLoginIntegrationApp/ViewController.swift
@@ -24,36 +24,33 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         loginButton.center = view.center
         loginButton.delegate = self
         
-        if(FBSDKAccessToken.current() != nil) {
+        if let token = FBSDKAccessToken.current() {
             fetchProfile()
         }
     }
     
-    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        
-        fetchProfile()
-        
-        print("******************************************************")
-        print("Completed Login 1")
-        print("******************************************************")
-    }
-    
     func fetchProfile() {
-        print("Fetch Profile")
+        print("Inside Fetch Profile")
             
         let parameters = ["fields":"email,first_name,last_name,picture.type(large)"] //set request permission
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: { (connection, result, error) -> Void in
+            
                 if error != nil {
                     print(error)
                     return
                 }
                 if let email = result as? [String:[AnyObject]]{
-                    print("******************************************************")
                     print(email) //print current login user's email in the console
-                    print("******************************************************")
                 }
             
-                
+                if let firtsName = result as? [String:[AnyObject]]{
+                    print(firtsName) //print current login user's email in the console
+                }
+            
+                if let lastName = result as? [String:[AnyObject]]{
+                    print(lastName) //print current login user's email in the console
+                }
+            
             })
             
         }
@@ -64,7 +61,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         fetchProfile()
         
         print("******************************************************")
-        print("Completed Login 2")
+        print("Completed Login")
         print("******************************************************")
     }
     
